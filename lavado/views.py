c76cc37d401cd234acbf4bc7ecc86c040d9b1098
Emiliano Perez <emiliano@anunciatedigital.com>
2020-02-18 17:02:55 -0600

    paginas redireccionadas correctamente

--- lavado/views.py
+++ lavado/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
-def home(request):
-    return render(request, 'home.html', {'name': 'Pipo'})
-
-
 def index(request):
     return render(request, 'index.html')
+
+def paquetes(request):
+    return render(request, 'paquetes.html')
