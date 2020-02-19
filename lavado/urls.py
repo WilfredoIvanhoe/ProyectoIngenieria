e66c606c55064771f7f3fe36fd0f7c360f7e85be
Emiliano Perez <emiliano@anunciatedigital.com>
2020-02-19 17:23:05 -0600

    functional dashboard

--- lavado/urls.py
+++ lavado/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index, name="index"),
     path('paquetes', views.paquetes, name="paquetes"),
     path('dashboard', views.dashboard, name="dashboard"),
 
