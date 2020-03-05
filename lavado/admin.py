010928f012fc94e06fe366077a4f7e5e4fac9987
Claire <jordan.965@hotmail.com>
2020-03-04 16:28:05 -0800

    XD

--- lavado/admin.py
+++ lavado/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Articulo, Empleado, Lavado, LavadoHasArticulo, LugarCompra, Paquete, RolEmpleado
+from .models import Articulo, Empleado, Lavado, LugarCompra, Paquete
 # Register your models here.
 admin.site.register(Empleado)
-admin.site.register(RolEmpleado)
 admin.site.register(Paquete)
 admin.site.register(LugarCompra)
-admin.site.register(LavadoHasArticulo)
 admin.site.register(Articulo)
